67c8eb39d0a8d9fac7f827576d4aa2d236ef7341
Elmir_Musaev <no_email@secret>
2023-04-05 01:59:55 +0400

    serious troubles in bubble_sort.py fixed

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -1,12 +1,13 @@
 my_arr = [9,8,7,6,5,4,3,2,1,0]
 
-for i in range(10):
+for i in range(9):
   for j in range(9):
     if (my_arr[j] > my_arr[j+1]):
       temp = my_arr[j+1]
       my_arr[j+1] = my_arr[j]
       my_arr[j] = temp
-      print(f"{i} ", end='')
-      break
+
+for x in range(len(my_arr)):
+  print(my_arr[x], "", end='')
 
 print("\n", end='')
