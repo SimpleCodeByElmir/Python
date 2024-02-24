5ddb3c81f58411be6b93a017b362bb6d4219331f
Elmir_Musaev <worldrap4@gmail.com>
2024-02-25 02:46:28 +0400

    buble sort, python, details

--- src/algorithms/bubble_sort.py
+++ src/algorithms/bubble_sort.py
@@ -1,7 +1,7 @@
 
 
 def bubble_sort(array):
-    for i in range(0, len(array)):
+    for i in range(0, len(array)-1):
         for j in range(0, len(array)-1):
             if array[j] > array[j+1]:
                 temp = array[j+1]
