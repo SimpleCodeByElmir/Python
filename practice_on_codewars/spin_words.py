55212182beac1d415986c2d7378bda2faf25dc5d
Elmir_Musaev <worldrap4@gmail.com>
2024-03-19 01:33:47 +0400

    spin_words.py - ready

--- practice_on_codewars/spin_words.py
+++ practice_on_codewars/spin_words.py
@@ -14,16 +14,34 @@
 
 def spin_words(string):
     new_str = ""
-    special_chars = ",.!?"
+    special_chars = ":,.!?"
     for word in string.split():
         if len(word) >= 5:
             word = word[::-1]
-            #for i in range(0, len(word)):
-                #if word[i] in special_chars:
-                    #word.replace(word[i], '')
+            for i in range(0, len(word)):
+                if word[i] in special_chars:
+                    word = word.replace(word[i], '') + word[i]  # moving ":,.!?" in the end.
+                    break
         new_str += word + " "
-    return new_str.strip()    
+    return new_str.strip()
 
 string = "Stop Snipping My Words!"
 print(spin_words(string))  # Stop gninnipS My sdroW!
+
+
+# More "gentle" solution from another User on the codewars.com
+
+#1
+#def spin_words(sentence):
+#    return " ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")])
+
+#2
+#def spin_words(sentence):
+#    words = [word for word in sentence.split(" ")]
+#    words = [word if len(word) < 5 else word[::-1] for word in words]
+#    return " ".join(words)
+
+#3
+#def spin_words(sentence):
+#    return ' '.join(word if len(word)<5 else word[::-1] for word in sentence.split())
 
