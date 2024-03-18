a56758d9a64ddcf7bf96c00b1be18fc53dc70a2e
Elmir_Musaev <worldrap4@gmail.com>
2024-03-19 01:18:02 +0400

    spin_words needs to thing how to move '\!' or '?' in the end of a sentence

--- practice_on_codewars/spin_words.py
+++ practice_on_codewars/spin_words.py
@@ -15,19 +15,15 @@
 def spin_words(string):
     new_str = ""
     special_chars = ",.!?"
-    #special_chars = "@_!#$%^&*()<>?}{~:]\"\'"
     for word in string.split():
         if len(word) >= 5:
             word = word[::-1]
-            for i in range(0, len(word)):
-                if word[i] in special_chars:
-                    #print(word[i])
-                    word.replace('!', 'X')
+            #for i in range(0, len(word)):
+                #if word[i] in special_chars:
                     #word.replace(word[i], '')
         new_str += word + " "
-    return new_str
+    return new_str.strip()    
 
 string = "Stop Snipping My Words!"
-print(spin_words(string))
-
+print(spin_words(string))  # Stop gninnipS My sdroW!
 
